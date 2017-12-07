3bc47f40807a12deb3a97fa295f56d9e6b0430f6
Martins Somoye <martins@somoyetosin.com>
2017-12-07 02:14:24 -0500

    Change made

--- app.js
+++ app.js
@@ -5,13 +5,40 @@ var express = require("express");
 var app = express();
 //importing routes from routes.js
 var routes = require("./routes");
+//importing the model from models.js
+//var model = require("./models");
 //importing body parser for json
 var jsonParser = require("body-parser").json;
 //importing morgan for HTTP request logger middleware 
 var logger = require("morgan");
 app.use(logger("dev"));
-
 app.use(jsonParser());
+//mongoDB connection
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/qa");
+
+var db = mongoose.connection;
+
+db.on("error", function(err){
+    console.error("connection error:", err);
+});
+
+db.once("open", function(err){
+    console.log("connection successful");
+    //All database communication goes here
+});
+
+//middleware for cross origin request
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+        return res.status(200).json({});
+    }
+    next();
+})
 //routes starting with questions
 app.use("/questions", routes);
 
